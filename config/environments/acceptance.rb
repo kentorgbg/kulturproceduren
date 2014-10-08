294226592c1a631bf8cea5349eca21c124bfd5ac
Patrik Johansson <patrik.j.johansson@kentor.se>
2014-10-08 23:19:49 +0200

    Fungerade inte med assets och wysiwyg.js

--- config/environments/acceptance.rb
+++ config/environments/acceptance.rb
@@ -47,8 +47,9 @@ Kulturproceduren::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( wysiwyg.js )
+  # config.assets.precompile += %w( search.js )
 
+  config.assets.precompile += %w( wysiwyg.js )
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
 
