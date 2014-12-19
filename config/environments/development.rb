25886d45a6b7de1eb1e5d3fee3a57d9aeb5e997b
Nils-Gunnar Nilsson <nils-gunnar.nilsson@kentor.se>
2014-12-19 11:04:30 +0100

    attempt to fix assets pipeline for acceptance and extra mail-reciepent

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,20 @@ Kulturproceduren::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # start BUG FIX SEARCH
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = true
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( tabbed_content.js )
+  # stop BUG FIX SEARCH
+
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
