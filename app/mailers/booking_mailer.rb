2f2f4697aec7c4ade41e8bf4d13b1d578900f141
Nils-Gunnar Nilsson <nils-gunnar.nilsson@kentor.se>
2015-01-16 13:32:08 +0100

    Send email to admins when changing or setting speciella krav on a booking

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -24,4 +24,23 @@ class BookingMailer < ActionMailer::Base
       subject: "Kulturproceduren: Avbokning - #{booking.group.name}, #{booking.group.school.name} till #{booking.occasion.event.name}"
     )
   end
+
+  # Sends an email to administrators when a booking has booking_requirements added or changed
+  def booking_requirements_added_or_changed_email(administrators, user, booking, change)
+    if APP_CONFIG[:mailers][:debug_recipient]
+      recipients = APP_CONFIG[:mailers][:debug_recipient]
+    else
+      recipients = administrators.map { |admin| admin.email }
+    end
+
+    @user        = user
+    @booking     = booking
+
+    mail(
+        to: recipients,
+        date: Time.zone.now,
+        subject: "Kulturproceduren: Bokning med" + (change ? " ändrade " : " ") + "Speciella krav/Extra information - #{booking.group.name}, #{booking.group.school.name} till #{booking.occasion.event.name}"
+    )
+  end
+
 end
